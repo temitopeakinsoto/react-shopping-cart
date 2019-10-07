81f43298611620a847f4f07046879d7ea5653179
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-07 14:43:52 +0100

    Modify Shopping cart Route in app.js
    In this commit, the <ShoppingCart /> Route in app.js is modified to no longer
    pass value via props into the Shopping cart route

--- src/App.js
+++ src/App.js
@@ -25,11 +25,8 @@ function App() {
 				<CartContext.Provider value={cart}>						
 					<Navigation cart={cart} />
 					{/* Routes */}			
-					<Route exact path="/" component={Products} 	/>			
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/>
+					<Route exact path="/" component={Products} />			
+					<Route path="/cart" component={ShoppingCart} />
 				</CartContext.Provider>				
 			</ProductContext.Provider>
 		</div>
