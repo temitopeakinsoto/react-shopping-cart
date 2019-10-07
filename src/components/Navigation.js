a3962059e5638360055b5f12b380576195d5f1a9
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-07 14:50:21 +0100

    feat: Consume data from CartContext in <Navigation /> component
    In this commit, the data within CartContext is being consumed in the navigation component.
    In achieving this, useContext is imported from the react lib and CartContext is imported
    from the CartContex.js file.The cart data store in CartContext is then retrieved by passing CartContext into the invocation of useContext

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import CartContext from '../contexts/CartContext';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = props => {
+	const {cart} = useContext(CartContext);
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
